465369c3e1efc837a1b142551bb61cd79a8629b3
Charles PAMART <charles.pamart@quartz-insight.com>
2020-05-26 20:08:18 +0200

    Each ToutDouxTask has now a Url property

--- src/ToutDoux/Models/ToutDouxTask.cs
+++ src/ToutDoux/Models/ToutDouxTask.cs
@@ -5,6 +5,7 @@
         public long Id{ get; set; }
         public string Title { get; set; }
         public bool Completed { get; set; }
+        public string Url { get; set; }
     }
 
     public class ToutDouxTaskForCreationDTO
